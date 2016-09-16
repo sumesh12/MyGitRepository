69a4563b14e9d18e6e6790d2fa1c007f366b097a
sumesh12 <S525740@mail.nwmissouri.edu>
2016-09-16 12:32:45 -0500

    Added a line and committed

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -37,6 +37,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
+        "hello",
         "abstraction",
         "ambiguous",
         "arithmetic",
@@ -85,6 +86,7 @@ final class StaticWordLibrary extends WordLibrary {
         "AjayBandi"};
 
     private static final String[] SCRAMBLED_WORD_LIST = {
+        "lleho",
         "batsartcoin",
         "maibuguos",
         "ratimhteci",
